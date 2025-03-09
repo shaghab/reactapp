a79974b69dccab5cb0cdae93111c1d771fb7b7ce
Asim Ghaffar <asim.ghaffar@gmail.com>
2025-03-09 16:46:10 +0500

    Refactor Counter component to remove timer and use button for incrementing count

--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function Counter({ init, end }) {
   init = parseInt(init || 0);
   end = parseInt(end || 0);
   const [count, setCount] = useState(init);
 
-  useEffect(function () {
-    if (end && count >= end) return;
-    var timer = setInterval(function () {
-      setCount((count) => count + 1);
-      console.log('count =' + count);
-    }, 1000);
+  function incr() {
+    setCount((count) => count + 1);
+  }
 
-    // If a function is returned by useEffect(),
-    // it allows performing a treatment
-    // before each component update
-    return function () {
-      clearInterval(timer);
-    };
-  });
   return (
     <>
       Initial value of the counter is: {init}
@@ -27,12 +17,13 @@ function Counter({ init, end }) {
       <br />
       The counter is set to: {count}
       <br />
-      {end && count >= end ? (
-        <b style={{ color: 'red', fontSize: '20px' }}>
-          Counter stopped
-        </b>
+      {count < end ? (
+        <>
+          <i>Counter in progress</i>
+          <button onClick={incr}>count+1</button>
+        </>
       ) : (
-        <i>Counter in progress</i>
+        <b>Counter stopped</b>
       )}
     </>
   );
