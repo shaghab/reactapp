99363cdf23a2e35e50fb8f0d4bd4e01ba9a5c7c6
Asim Ghaffar <asim.ghaffar@gmail.com>
2025-03-09 16:08:58 +0500

    Using the “key” Attribute in JSX Elements Displayed by a Loop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter.js';
+import React from 'react';
 
 function App() {
   return (
@@ -9,13 +10,13 @@ function App() {
         var jsx = [];
         for (var i = 0; i < 3; i++) {
           jsx.push(
-            <>
+            <React.Fragment key={i}>
               Counter {i} defined by{' '}
               {`<Counter init='5' end='${10 + i}' />`} : <br />
               <Counter init="5" end={10 + i} />
               <br />
               <br />
-            </>
+            </React.Fragment>
           );
         }
         return jsx;
