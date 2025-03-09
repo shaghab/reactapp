0c03421f841d57e3b56b300b91fd68c10931cd98
Asim Ghaffar <asim.ghaffar@gmail.com>
2025-03-09 12:33:05 +0500

    Incrementing the counter with setInterval()

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,9 @@
 function Counter() {
   var count = 0;
+  setInterval(function () {
+    count++;
+    console.log('count =' + count);
+  }, 1000);
   return <>The counter is set to: {count}</>;
 }
 
