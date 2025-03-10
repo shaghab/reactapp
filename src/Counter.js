fce9fc74b7a32f180b093d3493e8614277557ad4
Asim Ghaffar <asim.ghaffar@gmail.com>
2025-03-10 05:23:22 +0500

    Add keydown event handler to restrict input to digits

--- src/Counter.js
+++ src/Counter.js
@@ -7,9 +7,18 @@ function Counter() {
     setCount(event.target.value);
   }
 
+  function keydown(event) {
+    // Display the pressed key in the console
+    console.log(event.key);
+    // Allow the Backspace, Delete, ArrowLeft, ArrowRight, and Tab keys
+    if (['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'].includes(event.key)) return;
+    // Then disallow all other keys except the 0 to 9
+    if (event.key < '0' || event.key > '9') event.preventDefault();
+  }
+
   return (
     <>
-      Counter: <input type="text" onChange={change} /> <br />
+      Counter: <input type="text" onChange={change} onKeyDown={keydown} /> <br />
       <br />
       Input value: {value}
     </>
