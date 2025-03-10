42ce5ba7db76b9ba09f00b0f2998ddf30ae2bd14
Asim Ghaffar <asim.ghaffar@gmail.com>
2025-03-10 05:12:38 +0500

    Replace to implement input for dynamic value

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import React from 'react';
 function App() {
   return (
     <>
-      {[...Array(3).keys()].map(function (i) {
-        var val = i < 2 ? '1' : '0'; // value of the "autostart" attribute
-        return (
-          <React.Fragment key={i}>
-            Counter {i} defined by
-            {`<Counter autostart='${val}' init='5' end='${10 + i}' />`} : <br />
-            <Counter autostart={val} init="5" end={10 + i} />
-            <br />
-            <br />
-          </React.Fragment>
-        );
-      })}
+      <Counter />
     </>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,56 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-function Counter({ init, end, autostart }) {
-  init = parseInt(init || 0);
-  end = parseInt(end || 0);
-  autostart = parseInt(autostart || 0);
-  const [count, setCount] = useState(init);
-  const [start, setStart] = useState(true); // true for displaying the Start button
+function Counter() {
+  const [value, setCount] = useState('');
 
-  useEffect(function () {
-    if (!start) {
-      // The Start button is not displayed; you can initiate the timer.
-      var timer = setInterval(function () {
-        setCount((count) => {
-          var newCount = count + 1;
-          if (newCount >= end) setStart(true);
-          return newCount;
-        });
-      }, 1000);
-    }
-
-    return function () {
-      clearInterval(timer);
-    };
-  });
-
-  useEffect(function () {
-    if (autostart) restart();
-  }, []);
-
-  function restart() {
-    setStart(false); // Hide the Start button
-    setCount(init); // Reset "count" to the initial value.
+  function change(event) {
+    setCount(event.target.value);
   }
 
   return (
     <>
-      Initial value of the counter is: {init}
+      Counter: <input type="text" onChange={change} /> <br />
       <br />
-      End of the counter at: {end}
-      <br />
-      The counter is: {count}
-      <br />
-      {start ? (
-        <>
-          <b>Counter stopped</b>&nbsp;
-          <button onClick={restart}>Start</button>
-        </>
-      ) : (
-        <i>Counter in progress</i>
-      )}
+      Input value: {value}
     </>
   );
 }
-
 export default Counter;
